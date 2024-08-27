bec1b4bdfad631edcbbbfea50fcec06ddb0c7fd4
nicholasmontanez10 <nicholas.montanez10@gmail.com>
2024-08-27 15:15:42 -0500

    update -7

--- index.js
+++ index.js
@@ -1,14 +1,8 @@
-// Get visit count from local storage or set to 0
-let visitCount = localStorage.getItem('visitCount') || 0;
-visitCount = parseInt(visitCount);
-
-// Update visit count on the webpage
-const visitCountElement = document.getElementById('visit-count');
-visitCountElement.textContent = visitCount;
-
-// Increment visit count and update on the webpage
-visitCount++;
-localStorage.setItem('visitCount', visitCount.toString());
+const viewCountElement = document.getElementById('viewCount');
+let views = localStorage.getItem('viewCounter') || 0;
+views++;
+localStorage.setItem('viewCounter', views);
+viewCountElement.textContent = views;
 
 function toggleExperience() {
     const experienceSection = document.querySelector('.resume-container');
