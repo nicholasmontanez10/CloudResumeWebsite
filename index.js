31ae19f804a6dff580c26f03774adc99b46825f6
nicholasmontanez10 <nicholas.montanez10@gmail.com>
2024-08-26 22:57:11 -0500

    initial commit

---
+++ index.js
@@ -0,0 +1,11 @@
+// Get visit count from local storage or set to 0
+let visitCount = localStorage.getItem('visitCount') || 0;
+visitCount = parseInt(visitCount);
+
+// Update visit count on the webpage
+const visitCountElement = document.getElementById('visit-count');
+visitCountElement.textContent = visitCount;
+
+// Increment visit count and update on the webpage
+visitCount++;
+localStorage.setItem('visitCount', visitCount.toString());
